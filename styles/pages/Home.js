88b91594d818a591b557257f7a40c8670f43569f
nypeeh <lammerlammer@outlook.com>
2021-03-03 14:09:06 -0300

    Finished MoveIt

--- styles/pages/Home.js
+++ styles/pages/Home.js
@@ -10,14 +10,20 @@ export const Container = styled.div`
 
     section {
       flex: 1;
-
       display: grid;
       grid-template-columns: 1fr 1fr;
       gap: 6.25rem;
       align-content: center;
     }
-`;
 
-export const LeftContainer = styled.div`
+    @media (max-width: 670px) {
+      flex: 1;
 
+      section {
+        flex-direction: column;
+        grid-template-columns: minmax(280px, 1fr 1fr);
+
+      }
+
+    }
 `;
